87979c1764abde568a584a99971f152d0eb5c72d
Justin Holguin <juxtin@github.com>
2021-12-10 09:37:40 -0800

    Add some aliases and git config

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
 cat .profile >> ~/.profile
+cat .sh_aliases >> ~/.profile
+
+# Git aliases
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.unstage 'reset HEAD'
+git config --global alias.ls 'log --pretty=format:"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate'
+git config --global alias.ll 'log --pretty=format:"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate --numstat'
+git config --global alias.up '!git remote update -p; git merge --ff-only @{u}'
+git config --global alias.pushup 'push -u origin HEAD'
+git conifg --global alias.fuckthis '!git reset --hard && git clean -fd && git checkout .'
+
+# Git config
+git config --global core.editor "vim"
